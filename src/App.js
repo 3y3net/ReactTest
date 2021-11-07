635dc9dae3a0f7c4c7b7a6fff697924e8620e37f
3y3net <javi@GameStation.localdomain>
2021-11-07 19:32:48 +0100

    Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+//Necesita npm install react-router-dom
 import React, {Component} from 'react';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import './App.css';
 
 import tasks from './sample/tasks.json';
 import Tasks from './components/Tasks';
 import TaskForm from './components/TaskForm';
+import Post from './components/Post'
 
 class App extends Component {
 
@@ -30,7 +33,7 @@ deleteTask = (id) => {
 
 checkDone = (id) => {
   const newTask = this.state.tasks.filter(task => {
-   if(task.id == id) {
+   if(task.id === id) {
     task.done = !task.done;
    }
    return task;
@@ -38,12 +41,29 @@ checkDone = (id) => {
   this.setState ({tasks: newTask})
 }
 
+Home = () => {
+  return (
+    <div>
+      <TaskForm addTask={this.addTask}/>
+      <br />
+      <br />
+      <Tasks 
+              tasks={this.state.tasks}
+              deleteTask ={this.deleteTask}
+              checkDone={this.checkDone}/>
+    </div>
+  );
+}
 
   render() {
-
     return <div>
-      <TaskForm addTask={this.addTask}/>
-      <Tasks tasks={this.state.tasks} deleteTask ={this.deleteTask} checkDone={this.checkDone}/>
+      <BrowserRouter>
+        <Link to="/">[Home]</Link> <Link to="/post">[post]</Link>
+        <Routes>
+            <Route exact path='/' element={<this.Home />} />
+              <Route exact path='/post' element={<Post/>}/>
+        </Routes>
+        </BrowserRouter>
     </div>
   }
 }
